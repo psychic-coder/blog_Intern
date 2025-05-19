ebad538f7d7446fd94cf6bf16ec93face294a0cd
psychic-coder <rohitganguly450@gmail.com>
2025-05-19 20:57:29 +0530

    changed the model a bit

--- Backend/models/blogModel.js
+++ Backend/models/blogModel.js
@@ -4,11 +4,9 @@ import mongoose from "mongoose";
 const blogSchema= new mongoose.Schema({
     title:{
         type:String,
-        required: true,
     },
     content:{
         type:String,    
-        required:true
     },
     tags:[{
         type:String,
