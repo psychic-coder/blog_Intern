88e00ec2674997462fa75bc47599764e6f12f294
psychic-coder <rohitganguly450@gmail.com>
2025-05-19 22:48:48 +0530

    made some changes

--- Backend/app.js
+++ Backend/app.js
@@ -34,7 +34,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend origin
+  origin: 'http://localhost:5173',
   credentials: true 
 
 }));app.use(morgan('dev'))
